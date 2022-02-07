58580145fbc5ccf8a8410e4a5ae65f0e1bbc3ef7
SMore-Napi <s.roma245@gmail.com>
2022-02-07 18:26:02 +0300

    Guided Project - Personality Quiz

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Роман Солдатов on 07.02.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+    
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
